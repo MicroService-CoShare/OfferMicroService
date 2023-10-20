738595d710cf9a251f4d0b210de63a324e9edde3
houssembalti <houssem.balti@esprit.tn>
2023-10-20 14:16:45 +0100

    update file name

--- src/main/java/tn/esprit/cosharemshoussem/Controllers/OfferController.java
+++ src/main/java/tn/esprit/cosharemshoussem/Controllers/OfferController.java
@@ -12,9 +12,15 @@ import tn.esprit.cosharemshoussem.Services.OfferService;
 @RestController
 
 public class OfferController {
+    private String title="hello , im the product microservice";
 
     @Autowired
     private OfferService offerService;
+    @RequestMapping("/hello")
+    public String sayHello(){
+        System.out.println(title);
+        return title;
+    }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Offer> addOffer(@RequestBody Offer offer) {
